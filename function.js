a652f327b7ce30ab9fba1af33f980adefac648ca
Brijesh Dungrani <brijeshdungrani303@gmail.com>
2021-07-07 02:40:07 +0200

    first commit

---
+++ function.js
@@ -0,0 +1,69 @@
+const emailId =   document.getElementById('emailId');
+const firstName =   document.getElementById('firstName');
+const lastName =   document.getElementById('lastName');
+const streetName =   document.getElementById('streetName');
+const houseNo =   document.getElementById('houseNo');
+const passWord = document.getElementById('passWord');
+const addBtn =   document.getElementById('addBtn');
+const updateBtn =   document.getElementById('updateBtn');
+const removeBtn =   document.getElementById('removeBtn');
+
+const database = firebase.database();
+
+addBtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    
+
+    firebase.auth().createUserWithEmailAndPassword(emailId.value, passWord.value)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        prompt(error.message);
+        // ..
+        
+    });
+
+    database.ref('/users/'+houseNo.value).set({
+        first_name: firstName.value,
+        last_name: lastName.value,
+        street_name: streetName.value,
+        house_no: houseNo.value,
+        email_id: emailId.value,
+        password: passWord.value,
+        
+        
+
+    });
+    alert(userCredential.user)
+
+});
+
+
+
+
+
+  function signUp(event){
+      
+      var email = emailId.value;
+      var password = passWord.value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+    });
+  }
+
+
+  
