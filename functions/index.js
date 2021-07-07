7059d6f530c71367f05368700a32539714c66ac9
Brijesh Dungrani <brijeshdungrani303@gmail.com>
2021-07-07 05:14:07 +0200

    hello function

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,17 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
 //const functions = require('firebase-functions');
 //const admin = require('firebase-admin');
 //admin.initializeApp(functions.config().firebase);
 
 //var database = admin.database();
-
+/*
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
-});
+});  
+*/
