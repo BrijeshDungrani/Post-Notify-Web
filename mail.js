63ebe3adf91892a00c6539cdb0a43491580ccc0a
Brijesh Dungrani <brijeshdungrani303@gmail.com>
2021-07-08 10:34:51 +0200

    mail.js error

---
+++ mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user:'parthsaliya86',
+        pass:'Sph@8448'
+    }
+});
+
+var mailoption = {
+    from: 'parthsaliya86@gmail.com',
+    to: 'brijeshdungrani07@gmail.com',
+    subject: 'Notification',
+    text: 'Hello'
+};
+
+transporter.sendMail(mailoption, function(error,info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('email sent');
+    }
+});
