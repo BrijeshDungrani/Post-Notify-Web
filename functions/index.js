cea9d5db401ae4c02546b2364f0b355174fb67e1
Brijesh Dungrani <brijeshdungrani303@gmail.com>
2021-07-10 14:16:19 +0200

    change

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,22 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 const database = admin.database();
 const rootRef = database.ref("User");
+
+console.log("start");
+/* const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "parthsaliya86",
+    pass: "sph@8448",
+  },
+});
+const mailoption = {
+  from: "parthsaliya86@gmail.com",
+  to: "brijeshdungrani303@gmail.com",
+  subject: "Notification",
+  text: "Hello",
+}; */
 /* eslint-disable eol-last */
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -22,35 +38,49 @@ exports.makeLppercase = functions.database.ref("/messages/{pushId}/original")
     });
 
 
-exports.newNodeDetected = functions.database.ref("User/{userId}/Notify")
-    .onWrite((change, context) => {
+exports.newNodeDetected = functions.database.ref("User/U1/Notify")
+    .onUpdate((change, context) => {
       const newval = change.after.val();
-      if (newval) {
-        rootRef.on("value", (snapshot) => {
-          const emailadd = snapshot.child("U1/emailss").val();
-          /* const nodemailer = require("nodemailer");
-          const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: "parthsaliya86",
-              pass: "sph@8448",
-            },
-          });
-          const mailoption = {
-            from: "parthsaliya86@gmail.com",
-            to: emailadd,
-            subject: "Notification",
-            text: "Hello",
-          };
-          transporter.sendMail(mailoption, function(error, info) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log("email sent");
-            }
-          }); */
-          
-          return database.ref("metadata/lc/").set(emailadd);
+      console.log("starts", newval);
+      // if (newval) {
+      rootRef.on("value", (snapshot) => {
+        console.log("snapshhhhhot", snapshot);
+        const emailadd = snapshot.child("U1/emailss").val();
+        const nodemailer = require("nodemailer");
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "parthsaliya86",
+            pass: "sph@8448",
+          },
         });
-      }
+        const mailoption = {
+          from: "parthsaliya86@gmail.com",
+          to: "brijeshdungrani303@gmail.com",
+          subject: "Notification",
+          text: "Hello",
+        };
+        transporter.sendMail(mailoption, function(error, info) {
+          console.log("Email function st");
+          if (error) {
+            console.log("email not sent", error);
+          } else {
+            console.log("email sent");
+          }
+        });
+        return database.ref("metadata/lc/").set(emailadd);
+        // return sendMail(emailadd);
+      });
+      // }
     });
+// to send
+/* function sendMail(email) {
+  return transporter.
+      sendMail(mailoption, function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("email sent");
+        }
+      });
+} */
